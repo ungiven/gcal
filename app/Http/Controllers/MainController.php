dcf50d678e50f8fd9c09a35da8f6915453503d30
PB <per.boda@gmail.com>
2020-10-05 08:25:52 +0200

    hejo

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//vendor\google\apiclient\src\Google;
+
+use Illuminate\Http\Request;
+
+
+//require base_path() . '/vendor/autoload.php';
+
+
+//* Returns an authorized API client.
+//* return Google_Client the authorized client object
+
+/*function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Calendar API PHP Quickstart');
+    $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
+    $client->setAuthConfig(base_path() . '/credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = base_path() . '/token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("%s", $authUrl);
+            //print 'Enter verification code: ';
+            //$authCode = trim(fgets(STDIN));
+
+            $authCode = "4/4wFrJqmnopd-E1alaB7autLJbE8ItEJBBe9bfLvCHaipZ7L3CZ1vwJk";
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Calendar($client);
+
+// Print the next 10 events on the user's calendar.
+$calendarId = 'primary';
+$optParams = array(
+    'maxResults' => 10,
+    'orderBy' => 'startTime',
+    'singleEvents' => true,
+    'timeMin' => date('c'),
+);
+$results = $service->events->listEvents($calendarId, $optParams);
+$events = $results->getItems();
+
+if (empty($events)) {
+    print "No upcoming events found.\n";
+} else {
+    print "Upcoming events:\n";
+    foreach ($events as $event) {
+        $start = $event->start->dateTime;
+        if (empty($start)) {
+            $start = $event->start->date;
+        }
+        printf("%s (%s)\n", $event->getSummary(), $start);
+    }
+}*/
+
+class MainController extends Controller
+{
+    private $calendar;
+
+    function __construct()
+    {
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Calendar API PHP Quickstart');
+        $client->setScopes(\Google_Service_Calendar::CALENDAR_READONLY);
+        $client->setAuthConfig(base_path() . '/credentials.json');
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = base_path() . '/token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("%s", $authUrl);
+                //print 'Enter verification code: ';
+                //$authCode = trim(fgets(STDIN));
+
+                $authCode = "4/4wFrJqmnopd-E1alaB7autLJbE8ItEJBBe9bfLvCHaipZ7L3CZ1vwJk";
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new \Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+
+        $this->calendar = new \Google_Service_Calendar($client);
+    }
+
+
+    public function start()
+    {
+        $calendarId = 'primary';
+        $optParams = array(
+            'maxResults' => 10,
+            'orderBy' => 'startTime',
+            'singleEvents' => true,
+            'timeMin' => date('c'),
+        );
+        $results = $this->calendar->events->listEvents($calendarId, $optParams);
+        $events = $results->getItems();
+
+        /*if (empty($events)) {
+            print "No upcoming events found.\n";
+        } else {
+            print "Upcoming events:\n";
+            foreach ($events as $event) {
+                $start = $event->start->dateTime;
+                if (empty($start)) {
+                    $start = $event->start->date;
+                }
+                printf("%s (%s)\n", $event->getSummary(), $start);
+            }
+        }*/
+
+        $r = [];
+        array_push($r, $events[0]->summary);
+        array_push($r, $events[0]->summary);
+
+
+        return var_dump($events[0]);
+
+        $data = [
+            'title' => 'Title',
+            'text' => 'ASDF',
+
+        ];
+
+        //return view('start')->with($data);
+    }
+
+    public function add()
+    {
+        return 'Add to IT';
+    }
+
+    public function update($id)
+    {
+        return 'Updated id: ' . $id;
+    }
+
+    public function delete()
+    {
+        return 'Deleted!';
+    }
+
+    private function getClient()
+    {
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Calendar API PHP Quickstart');
+        $client->setScopes(\Google_Service_Calendar::CALENDAR_READONLY);
+        $client->setAuthConfig(base_path() . '/credentials.json');
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = base_path() . '/token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("%s", $authUrl);
+                //print 'Enter verification code: ';
+                //$authCode = trim(fgets(STDIN));
+
+                $authCode = "4/4wFrJqmnopd-E1alaB7autLJbE8ItEJBBe9bfLvCHaipZ7L3CZ1vwJk";
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new \Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
+    }
+}
