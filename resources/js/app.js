c3b9290f15b8efa9f1f24d9c0e05d7a1a761f086
PB <per.boda@gmail.com>
2020-10-06 18:25:19 +0200

    removed old files

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,6 @@ require("./bootstrap");
 
 window.Vue = require("vue");
 
-Vue.component(
-    "example-component",
-    require("./components/ExampleComponent.vue").default
-);
-
 Vue.component("calendar", require("./components/Calendar.vue").default);
 
 Vue.component(
